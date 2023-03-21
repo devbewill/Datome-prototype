63b418e13ba8bf5abc14d67bb870374259ec96c4
devbewill <sedprc@gmail.com>
2023-03-21 15:55:12 +0100

    added first section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import Section from "@/components/Section";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <main>
         <Navbar></Navbar>
+        <Section padded></Section>
       </main>
     </>
   );
